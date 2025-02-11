4f28c866180f0abb8bc395af92abd01c2e1b39ce
Tanya301 <tanya@samokhvalov.com>
2025-02-11 14:18:11 -0800

    fetch data

---
+++ global.js
@@ -0,0 +1,13 @@
+// Fetch the data
+d3.csv("Mouse_Data_Student_Copy.xlsx - Fem Act.csv").then(data => {
+    console.log(data);
+});
+d3.csv("Mouse_Data_Student_Copy.xlsx - Fem Temp.csv").then(data => {
+    console.log(data);
+});
+d3.csv("Mouse_Data_Student_Copy.xlsx - Male Act.csv").then(data => {
+    console.log(data);
+});
+d3.csv("Mouse_Data_Student_Copy.xlsx - Male Temp.csv").then(data => {
+    console.log(data);
+});
