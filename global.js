8c8db372eb430a33f1c838eb97d9cc3a3087ee40
Tanya301 <tanya@samokhvalov.com>
2025-02-11 14:30:24 -0800

    fetching data should be ok now

--- global.js
+++ global.js
@@ -1,13 +1,28 @@
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
+
+// Your variables
+let fem_act = null;
+let fem_temp = null;
+let male_act = null;
+let male_temp = null;
+
 // Fetch the data
-d3.csv("Mouse_Data_Student_Copy.xlsx - Fem Act.csv").then(data => {
-    console.log(data);
-});
-d3.csv("Mouse_Data_Student_Copy.xlsx - Fem Temp.csv").then(data => {
-    console.log(data);
-});
-d3.csv("Mouse_Data_Student_Copy.xlsx - Male Act.csv").then(data => {
-    console.log(data);
-});
-d3.csv("Mouse_Data_Student_Copy.xlsx - Male Temp.csv").then(data => {
-    console.log(data);
-});
+d3.csv("./data/Mouse_Data_Student_Copy.xlsx - Fem Act.csv").then(data => {
+    fem_act = data;
+    console.log(fem_act);
+}).catch(error => console.error(error));
+
+d3.csv("./data/Mouse_Data_Student_Copy.xlsx - Fem Temp.csv").then(data => {
+    fem_temp = data;
+    console.log(fem_temp);
+}).catch(error => console.error(error));
+
+d3.csv("./data/Mouse_Data_Student_Copy.xlsx - Male Act.csv").then(data => {
+    male_act = data;
+    console.log(male_act);
+}).catch(error => console.error(error));
+
+d3.csv("./data/Mouse_Data_Student_Copy.xlsx - Male Temp.csv").then(data => {
+    male_temp = data;
+    console.log(male_temp);
+}).catch(error => console.error(error));
