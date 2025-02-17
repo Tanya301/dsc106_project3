5d7216ece744200cf179b792a715c652d794f355
Tanya301 <tanya@samokhvalov.com>
2025-02-17 12:56:16 -0800

    nav bar

--- global.js
+++ global.js
@@ -12,3 +12,33 @@ export const dataLoaded = Promise.all([
     d3.csv("./data/Mouse_Data_Student_Copy.xlsx - Male Act.csv").then(data => { male_act = data; }),
     d3.csv("./data/Mouse_Data_Student_Copy.xlsx - Male Temp.csv").then(data => { male_temp = data; })
 ]).then(() => ({ fem_act, fem_temp, male_act, male_temp }));
+
+// - - - Navigation - - - //
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'explore.html', title: 'Exploration' },
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    // Create link and add it to nav
+    if (!ARE_WE_HOME && !url.startsWith('http')) {
+        url = '../' + url;
+    }
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    if (a.host !== location.host) {
+        a.target = '_blank';
+    }
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+    }
+    nav.append(a);
+}
