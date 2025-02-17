59b676b418c209c4f3866621e2d23f0f85b280aa
Tanya301 <tanya@samokhvalov.com>
2025-02-17 13:04:37 -0800

    fix

--- global.js
+++ global.js
@@ -24,21 +24,28 @@ let pages = [
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 
+// Get the directory path from the current URL
+const currentPath = location.pathname.substring(0, location.pathname.lastIndexOf('/') + 1);
+
 for (let p of pages) {
     let url = p.url;
     let title = p.title;
-    // Create link and add it to nav
-    // if (!ARE_WE_HOME && !url.startsWith('http')) {
-    //     url = '../' + url;
-    // }
+    
+    // Construct the correct URL by combining current directory path with the page URL
+    url = currentPath + url;
+    
     let a = document.createElement('a');
     a.href = url;
     a.textContent = title;
+    
     if (a.host !== location.host) {
         a.target = '_blank';
     }
-    if (a.host === location.host && a.pathname === location.pathname) {
+    
+    // Check if this is the current page
+    if (a.host === location.host && location.pathname === a.pathname) {
         a.classList.add('current');
     }
+    
     nav.append(a);
 }
